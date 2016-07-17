85787c726b648455144da91cc0f4daea240b34b4
nicolayyus <nikatina@sibmail.com>
2016-07-17 19:00:17 +0600

    change

--- GitClone.py
+++ GitClone.py
@@ -3,14 +3,14 @@
 
 import subprocess
 
-# test message
 
 def main():
     print("Start!")
     print("----------------------------------")
-    subprocess.call('cd /home/cm/test_git/Lab_C/;echo "one" | git pull —no-edit https://github.com/nikolasj/Lab_C.git 1.0-pm', shell=True)
-    subprocess.call('cd /home/cm/test_git/Lab_C/;echo "one" | git branch 1.0-pm', shell=True)
-    subprocess.call('cd /home/cm/test_git/Lab_C/;echo "one" | git diff 1.0-pm^', shell=True)
+    subprocess.call('cd /home/nicolas/test_git/Lab_C/;echo "one" | git pull —no-edit https://github.com/nikolasj/Lab_C.git feature', shell=True)
+    subprocess.call('cd /home/nicolas/test_git/Lab_C/;echo "one" | touch Changes.txt', shell=True)
+    subprocess.call('cd /home/nicolas/test_git/Lab_C/;echo "one" | git checkout feature', shell=True)
+    subprocess.call('cd /home/nicolas/test_git/Lab_C/;echo "one" | git diff feature^ >> Changes.txt', shell=True)
     print("----------------------------------")
     print("Finish!")
 
